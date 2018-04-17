14e387147d4f2e39b36e9ee248f2abd90253796d
Ada Hutchings <adalynn.hutchings@gmail.com>
2018-04-16 22:38:44 -0700

    forgot to swtich branches

--- bloc-chat/src/App.js
+++ bloc-chat/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import * as firebase from 'firebase';
 import RoomList from './components/RoomList';
 import MessageList from './components/MessageList';
+import UserInfo from './components/UserInfo';
 
 // Initialize Firebase
 var config = {
